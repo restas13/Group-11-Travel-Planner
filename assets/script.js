4da443108fcdce3034a6d77587be11516e8088c1
Dennmed22 <medrano.dennis@yahoo.com>
2023-03-30 21:28:27 -0400

    added weather api

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,14 @@ fetch(countrylink)
             console.log(data);
         });
     });
+
+
+weatherLink = 'https://api.openweathermap.org/data/2.5/weather?q=orlando,32812&appid=73c15a19bebffba4ddf655737bbc2b28'
+
+fetch(weatherLink)
+    .then(function (response) {
+        console.log(response);
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
