5b762f7328a11076f8e55c76efd69ece248f2c8f
Jack Smith <jack_smith21554@yahoo.com>
2023-04-03 21:37:32 -0400

    Added api endpoint variations depending on drop-down criteria selection. Added click event listner to search button.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,54 @@
-countrylink = 'https://restcountries.com/v3.1/name/ecuador?fullText=true';
+var userText = document.querySelector('#country-input');
+var searchBtn = document.querySelector('#search-btn');
 
+var countrylink = "";
+    
+searchBtn.addEventListener('click', function(){
 
-fetch(countrylink)
-    .then(function (response) {
-        console.log(response);
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
+    // var searchText = userText.value;
+
+    // function fetchInfo(searchText) {
+    //     fetch(countrylink,
+    //         {
+    //             mode: 'no-cors',
+    //         })
+    //     .then(function(res) {
+    //         return res.json()
+    //     }).then(function(data) {
+    //         console.log(data);
+    //     })
+    // }
+
+    var nameSearch = function() {
+        countrylink.val = 'https://restcountries.com/v3.1/name/' + userText + '?fullText=true';
+    }
+
+    var continentSearch = function() {
+        countrylink.val = 'https://restcountries.com/v3.1/region/' + userText;
+    }
+
+    var languageSearch = function() {
+        countrylink.val = 'https://restcountries.com/v3.1/lang/' + userText;
+    }
+
+    $(document).ready(function() {
+        var criteriaSelection = $('select').find(':selected').val();
+    
+        if (criteriaSelection === 1) {
+            nameSearch();
+            return;
+        }
+        
+        if (criteriaSelection === 2) {
+            continentSearch();
+            return;
+        }
+        
+        if (criteriaSelection === 3) {
+            languageSearch();
+            return;
+        }
+    })
+
+    fetchInfo();
+})
