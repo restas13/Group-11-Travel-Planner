1de5a85a86a7d6ebc68b81212e5b720d4289fd90
Dennmed22 <medrano.dennis@yahoo.com>
2023-04-04 19:35:21 -0400

    weather api with button

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,24 @@
-countrylink = 'https://restcountries.com/v3.1/name/ecuador?fullText=true';
+const issueContainer = document.getElementById('issues');
+const fetchButton = document.getElementById('fetch-button');
+const inputval = document.querySelector('#cityinput')
+const city = document.querySelector('#cityoutput')
+const descrip = document.querySelector('#description')
+const temp = document.querySelector('#temp')
+const wind = document.querySelector('#wind')
 
 
 fetch(countrylink)
+    .then(function (response) {
+        console.log(response);
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
+
+
+weatherLink = 'https://api.openweathermap.org/data/2.5/weather?q=orlando,32812&appid=73c15a19bebffba4ddf655737bbc2b28'
+
+fetch(weatherLink)
     .then(function (response) {
         console.log(response);
         response.json().then(function (data) {
