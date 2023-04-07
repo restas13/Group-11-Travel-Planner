dba5491a77dcfe85054058325741a4c705fecc5d
Thien Trinh <trinh.c.thien@gmail.com>
2023-04-06 20:33:22 -0400

    Final hopefully apply Error Modal, Thien

--- countryassets/countries.js
+++ countryassets/countries.js
@@ -6,7 +6,26 @@ var previousSearch = document.querySelector('#searchedCountries');
 
 //creating the list of searchedcountries and setting the search type
 var searched = [];
-var searchType = 'continent';
+var searchType = 'currency';
+
+//Modal Start
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+//Modal End
 
 //setting the searched list if there are any searched countries in the local storage
 if (localStorage.getItem('searchedCountries')) {
@@ -100,6 +119,11 @@ function currencySearch(event) {
             //logging the data
             console.log(data);
 
+             //Error Modal for wrong input
+             if (response.status !== 200) {
+                modal.style.display = "block";
+                }
+
             //creating a ul element that will contain the list of countries
             var ul = document.createElement('ul')
 
@@ -155,6 +179,11 @@ function languageSearch(event) {
             //logging the data
             console.log(data);
 
+                 //Error Modal for wrong input
+                 if (response.status !== 200) {
+                    modal.style.display = "block";
+                    }
+
             //creating a ul element that will contain the list of countries
             var ul = document.createElement('ul')
 
@@ -209,6 +238,11 @@ function regionSearch(event) {
 
             //logging the data
             console.log(data);
+
+                 //Error Modal for wrong input
+                 if (response.status !== 200) {
+                    modal.style.display = "block";
+                    }
 
             //creating a ul element that will contain the list of countries
             var ul = document.createElement('ul')
